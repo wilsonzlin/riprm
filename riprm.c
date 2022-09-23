578196db0838a9e73560d6dccb606277ac77efcf
Wilson Lin <code@wilsonl.in>
2022-09-24 02:27:31 +1000

    Use io_uring for openat calls

--- riprm.c
+++ riprm.c
@@ -12,17 +12,120 @@
 // TODO Tune
 #define QUEUE_DEPTH 16
 #define DIR_OPEN_FLAGS (O_RDONLY | O_DIRECTORY)
+#define DIRENT_NAME_MAXLEN 256
+
+typedef struct {
+  int op;
+  int dir_fd;
+  char ent_name[DIRENT_NAME_MAXLEN];
+} task_t;
+
+task_t* task_create(int op, int dir_fd, char* ent_name) {
+  // TODO Bump allocate/buffer pool
+  task_t* task = malloc(sizeof(task_t));
+  task->op = op;
+  task->dir_fd = dir_fd;
+    // We must duplicate as next readdir can overwrite d_name.
+  memcpy(&task->ent_name[0], ent_name, DIRENT_NAME_MAXLEN);
+  return task;
+}
+
+void task_destroy(task_t* task) {
+  free(task);
+}
+
+void handle_cqe(
+  struct io_uring* ring,
+  int* queued,
+  struct io_uring_cqe *cqe
+);
+
+void visit_dir(
+  struct io_uring* ring,
+  int* queued,
+  int dir_fd
+);
+
+void submit(
+  struct io_uring* ring,
+  int* queued
+) {
+  io_uring_submit(ring);
+  struct io_uring_cqe* cqe;
+  int wait_err = io_uring_wait_cqe(ring, &cqe);
+  if (wait_err < 0) {
+    fprintf(stderr, "Failed to wait on queue: %s\n", strerror(-wait_err));
+    exit(1);
+  }
+  handle_cqe(ring, queued, cqe);
+  while (true) {
+    int peek_err = io_uring_peek_cqe(ring, &cqe);
+    if (peek_err == -EAGAIN) {
+      break;
+    }
+    if (peek_err < 0) {
+      fprintf(stderr, "Failed to peek queue: %s\n", strerror(-peek_err));
+      exit(1);
+    }
+    handle_cqe(ring, queued, cqe);
+  }
+}
+
+void increment_queued_and_maybe_submit(
+  struct io_uring* ring,
+  int* queued
+) {
+  (*queued)++;
+  if (*queued == QUEUE_DEPTH) {
+    submit(ring, queued);
+  }
+}
+
+void handle_openat_cqe(
+  struct io_uring* ring,
+  int* queued,
+  int dir_fd,
+  char* ent_name,
+  int subdir_fd
+) {
+  if (subdir_fd == -ENOTDIR) {
+    // TODO We assume it's a file.
+    struct io_uring_sqe *sqe = io_uring_get_sqe(ring);
+    // TODO We could reuse existing task.
+    task_t* task = task_create(IORING_OP_UNLINKAT, dir_fd, ent_name);
+    io_uring_prep_unlinkat(sqe, dir_fd, &task->ent_name[0], 0);
+    io_uring_sqe_set_data(sqe, task);
+    increment_queued_and_maybe_submit(ring, queued);
+  } else if (subdir_fd < 0) {
+    // Ignore inaccessible items.
+    // TODO Log?
+  } else {
+    visit_dir(ring, queued, subdir_fd);
+  }
+}
+
+void handle_unlinkat_cqe(
+  int result
+) {
+  // TODO Log errors
+  (void) result;
+}
 
 void handle_cqe(
   struct io_uring* ring,
   int* queued,
   struct io_uring_cqe *cqe
 ) {
-  char* file_name = io_uring_cqe_get_data(cqe);
-  // TODO Log errors
-  free(file_name);
+  task_t* task = io_uring_cqe_get_data(cqe);
+  // Immediately mark as seen and decrement count as we may try to enqueue new tasks in handlers.
   io_uring_cqe_seen(ring, cqe);
   (*queued)--;
+  if (task->op == IORING_OP_OPENAT) {
+    handle_openat_cqe(ring, queued, task->dir_fd, &task->ent_name[0], cqe->res);
+  } else {
+    handle_unlinkat_cqe(cqe->res);
+  }
+  task_destroy(task);
 }
 
 void visit_dir(
@@ -42,45 +145,13 @@ void visit_dir(
     if (!strcmp(ent->d_name, ".") || !strcmp(ent->d_name, "..")) {
       continue;
     }
+
     // d_type isn't set on some file systems (e.g. XFS with ftype=0); to avoid expensive stat, we try directly opening as dir.
-    int subdir_fd = openat(dir_fd, ent->d_name, DIR_OPEN_FLAGS);
-    // WARNING: Check return value first as only on error is `errno` set.
-    if (subdir_fd < 0 && errno == ENOTDIR) {
-      // TODO We assume it's a file.
-      struct io_uring_sqe *sqe = io_uring_get_sqe(ring);
-      // We must duplicate as next readdir can overwrite this data.
-      // TODO Bump allocate/buffer pool
-      char* file_name = strdup(ent->d_name);
-      io_uring_prep_unlinkat(sqe, dir_fd, file_name, 0);
-      io_uring_sqe_set_data(sqe, file_name);
-      (*queued)++;
-      if (*queued == QUEUE_DEPTH) {
-        io_uring_submit(ring);
-        struct io_uring_cqe* cqe;
-        int wait_err = io_uring_wait_cqe(ring, &cqe);
-        if (wait_err < 0) {
-          fprintf(stderr, "Failed to wait on queue: %s\n", strerror(-wait_err));
-          exit(1);
-        }
-        handle_cqe(ring, queued, cqe);
-        while (true) {
-          int peek_err = io_uring_peek_cqe(ring, &cqe);
-          if (peek_err == -EAGAIN) {
-            break;
-          }
-          if (peek_err < 0) {
-            fprintf(stderr, "Failed to peek queue: %s\n", strerror(-peek_err));
-            exit(1);
-          }
-          handle_cqe(ring, queued, cqe);
-        }
-      }
-    } else if (subdir_fd < 0) {
-      // Ignore inaccessible items.
-      // TODO Log?
-    } else {
-      visit_dir(ring, queued, subdir_fd);
-    }
+    task_t* task = task_create(IORING_OP_OPENAT, dir_fd, ent->d_name);
+    struct io_uring_sqe *sqe = io_uring_get_sqe(ring);
+    io_uring_prep_openat(sqe, dir_fd, &task->ent_name[0], DIR_OPEN_FLAGS, 0);
+    io_uring_sqe_set_data(sqe, task);
+    increment_queued_and_maybe_submit(ring, queued);
   }
 }
 
@@ -105,9 +176,7 @@ int main(int argc, char** argv) {
     }
     visit_dir(&ring, &queued, dir_fd);
   }
-  io_uring_submit(&ring);
-
-  // Since we currently don't do anything on completion (not even logging on errors), don't bother cleaning up everything (closing file descriptors, processing all completions, freeing all memory).
-  struct io_uring_cqe* cqe;
-  io_uring_wait_cqe_nr(&ring, &cqe, queued);
+  while (queued) {
+    submit(&ring, &queued);
+  }
 }
